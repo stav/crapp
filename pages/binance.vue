541c9ea5d340606d3829c03e1a835a9a6197fd56
Steven Almeroth <sroth77@gmail.com>
2020-08-25 19:29:15 -0400

    cleanups

--- pages/binance.vue
+++ pages/binance.vue
@@ -6,9 +6,7 @@
           :src="require('@/assets/logos-download.com/Binance_logo_coin.png')"
           max-width="27px"
         />
-        <v-toolbar-title class="mx-2">
-          Binance
-        </v-toolbar-title>
+        <v-toolbar-title class="mx-2"> Binance </v-toolbar-title>
         <v-spacer />
         <v-btn title="Clear" @click="clear"> X </v-btn>
         <v-btn @click="() => fetch('system.status')" color="secondary" title="Check the system status"> Status </v-btn>
@@ -20,21 +18,12 @@
           <v-col cols="12">
             <json-view :data="result" />
           </v-col>
-          <v-col
-            v-for="(balance, i) in balances"
-            :key="i"
-          >
+          <v-col v-for="(balance, i) in balances" :key="i">
             <v-card>
               <div class="d-flex flex-no-wrap justify-space-between">
                 <v-btn x-large block>
-                  <v-card-subtitle
-                    class="mx-0 px-0"
-                    v-text="balance.free"
-                  />
-                  <v-card-title
-                    class="headline mx-0 pl-1"
-                    v-text="balance.asset"
-                  />
+                  <v-card-subtitle class="mx-0 px-0" v-text="balance.free" />
+                  <v-card-title class="headline mx-0 pl-1" v-text="balance.asset" />
                   <v-card-subtitle
                     class="mx-0 pa-0"
                     v-if="balance.locked"
