06c7a02dbf9d127b0a497c7082c33e79abedc327
Steven Almeroth <sroth77@gmail.com>
2020-11-10 22:51:31 -0500

    exchange buttons now in repo bar

--- components/RepoBar.vue
+++ components/RepoBar.vue
@@ -1,35 +1,46 @@
 <template>
-  <v-app-bar color="indigo">
-    <v-icon class="mr-2">mdi-bitcoin</v-icon> Repositories <v-spacer />
-    {{ coins.length }} coins in {{ repositorys.length }} repos <v-spacer />
-    <v-card class="mx-auto" :loading="loading">
-      <v-card-actions v-if="repositorys.length">
-        <v-btn
-          @click="fetchAllSparks"
-          title="Press to fetch latest history prices displayed as sparklines"
-          small class="accent px-1"
-        >
-          Sparks
-        </v-btn>
-        <v-btn
-          @click="fetchPrices"
-          title="Press to fetch latest current prices"
-          small class="accent px-1"
-        >
-          Load Coin Data
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    <v-spacer />
-    <span @click="coinValue=false" class="clickable text--secondary"> amount </span>
-    <v-switch
-      v-model="coinValue"
-      class="ml-3 mr-2"
-      hide-details
-      title="Display amount of coins held or the USD valuation"
-    />
-    <span @click="coinValue=true" class="clickable text--secondary"> value </span>
-  </v-app-bar>
+  <div>
+    <v-app-bar color="indigo">
+      <v-icon class="mr-2">mdi-bitcoin</v-icon> Repositories <v-spacer />
+      {{ coins.length }} coins in {{ repositorys.length }} repos <v-spacer />
+      <v-card class="mx-auto" :loading="loading">
+        <v-card-actions v-if="repositorys.length">
+          <v-btn
+            @click="fetchAllSparks"
+            title="Press to fetch latest history prices displayed as sparklines"
+            small class="accent px-1"
+          >
+            Sparks
+          </v-btn>
+          <v-btn
+            @click="fetchPrices"
+            title="Press to fetch latest current prices"
+            small class="accent px-1"
+          >
+            Load Coin Data
+          </v-btn>
+          <v-btn @click="getBinanceAccountsData"> Binance </v-btn>
+          <v-btn @click="getCoinbaseAccountsData"> Coinbase </v-btn>
+          <v-btn @click="getCoinbaseProAccountsData"> Coinbase Pro </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-spacer />
+      <span @click="coinValue=false" class="clickable text--secondary"> amount </span>
+      <v-switch
+        v-model="coinValue"
+        class="ml-3 mr-2"
+        hide-details
+        title="Display amount of coins held or the USD valuation"
+      />
+      <span @click="coinValue=true" class="clickable text--secondary"> value </span>
+    </v-app-bar>
+    <v-snackbar v-model="snackbarModel" timeout="3000">
+      {{ snackbarText }}
+      <template v-slot:action="{ attrs }">
+        <v-btn dark text v-bind="attrs" @click="snackbarModel = false"> Close </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -60,6 +71,8 @@ export default {
   */
   data: () => ({
     loading: false,
+    snackbarText: '',
+    snackbarModel: false,
   }),
 
   /*
@@ -88,7 +101,24 @@ export default {
       this.loading = 'accent'
       this.$store.dispatch('fetchPrices', this.done)
     },
-    done () {
+    getCoinbaseProAccountsData () {
+      this.loading = 'blue'
+      this.$store.dispatch('loadCoinbaseProAccounts', this.done)
+    },
+    getCoinbaseAccountsData () {
+      this.loading = 'green'
+      this.$store.dispatch('loadCoinbaseAccounts', this.done)
+    },
+    getBinanceAccountsData () {
+      this.loading = 'yellow'
+      this.$store.dispatch('loadBinanceBalances', this.done)
+    },
+    done (message) {
+      console.debug(message)
+      if (message) {
+        this.snackbarText = message
+        this.snackbarModel = true
+      }
       this.loading = false
     },
   },
--- pages/repos.vue
+++ pages/repos.vue
@@ -1,15 +1,7 @@
 <template>
-  <v-card class="mx-auto" :loading="loading">
+  <v-card class="mx-auto">
     <repo-bar :repositorys="repositorys" :coins="symbols" />
-
     <repo-table :repositorys="repositorys" :coins="symbols" />
-
-    <v-snackbar v-model="snackbarModel" timeout="3000">
-      {{ snackbarText }}
-      <template v-slot:action="{ attrs }">
-        <v-btn dark text v-bind="attrs" @click="snackbarModel = false"> Close </v-btn>
-      </template>
-    </v-snackbar>
   </v-card>
 </template>
 
@@ -36,16 +28,6 @@ export default {
     this.$store.commit('setSelectedRepos', this.repositorys)
     this.$store.dispatch('fetchPrices')
   },
-
-  /*
-  ** DATA
-  */
-  data: () => ({
-    // timeout: null,
-    loading: false,
-    snackbarText: '',
-    snackbarModel: false,
-  }),
 
   /*
   ** COMPUTED
@@ -75,45 +57,6 @@ export default {
       return this.$store.getters.sortedUniqueSymbols()
     },
   },
-
-  /*
-  ** MOUNTED
-  */
-  mounted () {
-    this.snackbarText = 'Mounted'
-    this.snackbarModel = true
-    // this.timeout = setInterval(this.getCoinbaseAccountsData, 10000)
-  },
-
-  /*
-  ** METHODS
-  */
-  methods: {
-    getCoinbaseProAccountsData () {
-      this.loading = 'blue'
-      this.$store.dispatch('loadCoinbaseProAccounts', this.done)
-    },
-    getCoinbaseAccountsData () {
-      this.loading = 'yellow'
-      this.$store.dispatch('loadCoinbaseAccounts', this.done)
-    },
-    getBinanceAccountsData () {
-      this.loading = 'green'
-      this.$store.dispatch('loadBinanceBalances', this.done)
-    },
-    done (message) {
-      if (message) {
-        this.snackbarText = message
-        this.snackbarModel = true
-      }
-      this.loading = false
-    },
-  },
-
-  // beforeRouteLeave (to, from, next) {
-  //   clearTimeout(this.timeout)
-  //   next()
-  // },
 
 }
 </script>
