ce41a588acee918092ee198a693d7bbd0b217dd0
Steven Almeroth <sroth77@gmail.com>
2020-12-01 01:11:45 -0500

    charts: add units yaxis

--- pages/charts.vue
+++ pages/charts.vue
@@ -78,6 +78,7 @@ export default {
     const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart)
     chart.paddingRight = 20
     chart.cursor = new am4charts.XYCursor()
+    chart.legend = new am4charts.Legend()
 
     const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
     dateAxis.renderer.grid.template.location = 0
@@ -88,26 +89,43 @@ export default {
     valueAxis.extraTooltipPrecision = 1
     valueAxis.renderer.minWidth = 35
 
-    const series = chart.series.push(new am4charts.LineSeries())
-    series.name = 'Price History'
-    series.tooltipText = '$ {valueY.value}'
-    series.dataFields.dateX = 'date'
-    series.dataFields.valueY = 'value'
-
-    const series2 = chart.series.push(new am4charts.ColumnSeries())
-    series2.name = 'Units'
-    // series2.alwaysShowTooltip = true
-    series2.strokeWidth = 6
-    series2.dataFields.dateX = 'date'
-    series2.dataFields.valueY = 'units'
-    series2.stroke = am4core.color('#CDA2AB')
-    series2.columns.template.propertyFields.fill = 'color'
-    series2.columns.template.propertyFields.stroke = 'white'
-    series2.columns.template.tooltipText = '{type} on {repo}:\n[bold]{quantity}[/b]'
+    const priceSeries = chart.series.push(new am4charts.LineSeries())
+    priceSeries.name = 'Price History'
+    priceSeries.tooltipText = '$ {valueY.value}'
+    priceSeries.dataFields.dateX = 'date'
+    priceSeries.dataFields.valueY = 'value'
 
     const scrollbarX = new am4charts.XYChartScrollbar()
-    scrollbarX.series.push(series)
+    scrollbarX.series.push(priceSeries)
     chart.scrollbarX = scrollbarX
+
+    const unitsAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    unitsAxis.title.text = 'Units'
+    unitsAxis.renderer.opposite = true
+
+    const unitsSeries = chart.series.push(new am4charts.ColumnSeries())
+    unitsSeries.name = 'Units'
+    unitsSeries.yAxis = unitsAxis
+    unitsSeries.extraTooltipPrecision = 1
+    unitsSeries.dataFields.dateX = 'date'
+    unitsSeries.dataFields.valueY = 'units'
+    unitsSeries.stroke = am4core.color('#CDA2AB')
+    unitsSeries.strokeWidth = 4
+    unitsSeries.columns.template.showTooltipOn = 'hit'
+    unitsSeries.columns.template.propertyFields.fill = 'color'
+    unitsSeries.columns.template.propertyFields.stroke = 'white'
+    unitsSeries.columns.template.tooltipText = '{type} on {repo}:\n[bold]{quantity}[/]'
+    // unitsSeries.columns.template.width = am4core.percent(1000)
+    unitsSeries.columns.template.width = 32
+
+    chart.maskBullets = false
+    const labelBullet = unitsSeries.bullets.push(new am4charts.LabelBullet())
+    labelBullet.label.text = '{valueY}'
+    labelBullet.label.truncate = false
+    labelBullet.label.hideOversized = false
+    labelBullet.label.horizontalCenter = 'left'
+    labelBullet.label.dx = 18
+    // labelBullet.adapter.add('y', () => 15)
 
     this.chart = chart
     this.loadSymbol()
@@ -142,11 +160,11 @@ export default {
         // const coin = Coin.query().where('symbol', this.symbol).first()
         let t = 0
         for (let d = 0; d < data.length; d++) {
-          const { date: chartDate, value } = data[d]
+          const { date: chartDate } = data[d]
           const chartTimestamp = chartDate.getTime()
           const transTimestamp = trans[t].timestamp
           if (transTimestamp < chartTimestamp) {
-            data[d].units = value
+            data[d].units = trans[t].quantity
             data[d].type = trans[t].type
             data[d].repo = trans[t].repo
             data[d].quantity = trans[t].quantity
@@ -163,6 +181,7 @@ export default {
       if (trans.length) { addTransactions() }
       this.chart.data = data
     },
+
   },
 
 }
