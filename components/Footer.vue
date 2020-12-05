f9a7f465fccf665affecc5b46156675a140d55a5
Steven Almeroth <sroth77@gmail.com>
2020-12-04 22:43:41 -0500

    logos in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
   <v-footer app :absolute="footerAbsolute">
-    <span>&copy; {{ new Date().getFullYear() }}</span>
+    <span>&copy; {{ new Date().getFullYear() }} Steven Almeroth</span>
+    <v-spacer />
+    <v-img src="/logo-vuejs.png" max-width="20" max-height="20" class="pa-1" title="Powered by Vue.js" />
+    <v-img src="/logo-nuxt.png" max-width="20" max-height="20" class="pa-1" title="Powered by Nuxt" />
+    <v-img src="/logo-vuetify.png" max-width="20" max-height="20" class="pa-1" title="Powered by Vuetify" />
   </v-footer>
 </template>
 
