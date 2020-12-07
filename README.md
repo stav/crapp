e130f004d0b62b6dc34081851b2f126a2de691db
Steven Almeroth <sroth77@gmail.com>
2020-12-07 12:36:00 -0500

    percent change price history chart

--- README.md
+++ README.md
@@ -19,6 +19,24 @@ $ yarn generate
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
 
+## Deploy
+
+My host does not have enough available memory to build the app so I have to build
+it on the dev machine and copy the files over manually.
+
+	stav@varan ~/Work/Crypto/crapp$ cp -r .nuxt/ cowboy:/~/crapp/
+
+## Remote
+
+	stav@cowboy:/srv/~/crapp$ pm2 start 'yarn start' --name CrApp
+	[PM2] Starting /usr/bin/bash in fork_mode (1 instance)
+	[PM2] Done.
+	┌─────┬──────────┬─────────────┬─────────┬─────────┬──────────┬────────┬──────┬───────────┬──────────┬──────────┬──────────┬──────────┐
+	│ id  │ name     │ namespace   │ version │ mode    │ pid      │ uptime │ ↺    │ status    │ cpu      │ mem      │ user     │ watching │
+	├─────┼──────────┼─────────────┼─────────┼─────────┼──────────┼────────┼──────┼───────────┼──────────┼──────────┼──────────┼──────────┤
+	│ 0   │ CrApp    │ default     │ N/A     │ fork    │ 25117    │ 0s     │ 0    │ online    │ 0%       │ 696.0kb  │ stav     │ disabled │
+	└─────┴──────────┴─────────────┴─────────┴─────────┴──────────┴────────┴──────┴───────────┴──────────┴──────────┴──────────┴──────────┘
+
 ## Credits
 
 https://coindashboards.com/currency/uniswap
--- pages/history.vue
+++ pages/history.vue
@@ -75,14 +75,44 @@ export default {
   ** MOUNTED
   */
   mounted () {
+    // const keyboard = am4core.keyboard
+    // const interaction = am4core.getInteraction()
+    // const div = interaction.getInteraction(this.$refs.chartdiv)
+    // div.events.onAll((ev) => {
+    //   console.log('div:', ev)
+    // })
+    // console.log('interaction:', div)
+
     const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart)
-    chart.paddingRight = 20
-    chart.cursor = new am4charts.XYCursor()
     chart.legend = new am4charts.Legend()
+    chart.paddingRight = 20
+    // chart.plotContainer.tooltipPosition = 'pointer'
+    // chart.plotContainer.tooltipText = 'qwer'
+    // chart.plotContainer.adapter.add('tooltipText', function(text, target, key) {
+    //   console.log('axis', dateAxis.tooltipDate)
+    //   // console.log('plot', chart.yAxes.getIndex(0))
+    //   return 'texxt'
+    // })
+    // chart.events.onAll((ev) => {
+    //   console.log('chart event:', ev)
+    // })
 
     const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
     dateAxis.renderer.grid.template.location = 0
-    dateAxis.tooltipDateFormat = 'yyyy-MM-dd'
+    dateAxis.tooltipDateFormat = 'yyyy-MM-dd (h)'
+    // The following does the same thing as tooltipDateFormat
+    // dateAxis.adapter.add('getTooltipText', function(_text, target, _key) {
+    //   const d = target.tooltipDate
+    //   const Y = d.getFullYear()
+    //   const M = d.getMonth()
+    //   const D = d.getDate()
+    //   const h = d.getHours()
+    //   return `${Y}-${M}-${D} ${h}`
+    // })
+
+    // const percentValueAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    // percentValueAxis.title.text = 'Percent Change'
+    // percentValueAxis.renderer.labels.template.adapter.add('text', text => `${text} %`)
 
     const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
     valueAxis.title.text = 'Price (USD)'
@@ -90,10 +120,18 @@ export default {
     valueAxis.renderer.minWidth = 35
 
     const priceSeries = chart.series.push(new am4charts.LineSeries())
-    priceSeries.name = 'Price History'
+    priceSeries.name = 'Price'
     priceSeries.tooltipText = '$ {valueY.value}'
     priceSeries.dataFields.dateX = 'date'
     priceSeries.dataFields.valueY = 'value'
+    priceSeries.tooltip.label.adapter.add('text', (text, target) => target.dataItem?.valueY === 0 ? '' : text) // Don't show tooltip for zero-price data
+
+    const percentSeries = chart.series.push(new am4charts.StepLineSeries())
+    percentSeries.name = 'Change'
+    percentSeries.dataFields.dateX = 'date'
+    percentSeries.dataFields.valueY = 'value'
+    percentSeries.dataFields.valueYShow = 'changePercent'
+    percentSeries.tooltipText = '{valueY.changePercent}%'
 
     const scrollbarX = new am4charts.XYChartScrollbar()
     scrollbarX.series.push(priceSeries)
@@ -111,7 +149,7 @@ export default {
     unitsSeries.dataFields.valueY = 'units'
     unitsSeries.stroke = am4core.color('#CDA2AB')
     unitsSeries.strokeWidth = 4
-    unitsSeries.columns.template.showTooltipOn = 'hit'
+    unitsSeries.columns.template.showTooltipOn = 'always'
     unitsSeries.columns.template.propertyFields.fill = 'color'
     unitsSeries.columns.template.propertyFields.stroke = 'white'
     unitsSeries.columns.template.tooltipText = '{type} on {repo}:\n[bold]{quantity}[/]'
@@ -126,6 +164,21 @@ export default {
     labelBullet.label.horizontalCenter = 'left'
     labelBullet.label.dx = 18
     // labelBullet.adapter.add('y', () => 15)
+
+    const cursor = new am4charts.XYCursor()
+    cursor.behavior = 'zoomX'
+    cursor.tabindex = 1
+    // The following is used if we change: cursor.behavior = 'selectX'
+    // cursor.events.on('selectended', function(ev) {
+    //   const range = ev.target.xRange
+    //   const dateAxis = ev.target.chart.xAxes.getIndex(0)
+    //   const from = dateAxis.toAxisPosition(range.start)
+    //   const to = dateAxis.toAxisPosition(range.end)
+    //   // const from = dateAxis.getPositionLabel(dateAxis.toAxisPosition(range.start))
+    //   // const to = dateAxis.getPositionLabel(dateAxis.toAxisPosition(range.end))
+    //   console.log('Selected from ' + from + ' to ' + to)
+    // })
+    chart.cursor = cursor
 
     this.chart = chart
     this.loadSymbol()
