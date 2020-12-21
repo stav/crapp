53b7af020b72941e70b019f7ac6a1fe075ba6334
Steven Almeroth <sroth77@gmail.com>
2020-12-21 13:39:04 -0500

    better sparks ux

--- pages/sparks.vue
+++ pages/sparks.vue
@@ -1,22 +1,40 @@
 <template>
   <v-container>
-    <v-row no-gutters>
-      <v-col
-        v-for="symbol of symbols" :key="symbol"
-        @click="() => spark(symbol)"
-      >
-        <v-card class="ma-1" max-width="400">
-          <v-sheet :color="color(symbol)" width="400">
-            <span v-text="symbol" class="ml-2" />
-            <v-sparkline :value="values[symbol]" line-width="1" color="white" />
-          </v-sheet>
-        </v-card>
+    <v-row>
+      <v-col cols="2">
+        <v-btn
+          v-for="symbol of symbols" :key="symbol"
+          @click="() => spark(symbol)"
+          v-text="symbol"
+          :disabled="disabled(symbol)"
+          :color="color(symbol)"
+          class="mr-1 mb-1"
+        />
+      </v-col>
+      <v-col cols="10">
+        <v-container>
+          <v-row no-gutters>
+            <v-col
+              v-for="symbol in Object.keys(sparks)" :key="symbol"
+              @click="() => spark(symbol)"
+            >
+              <v-card class="ma-1" max-width="400">
+                <v-sheet :color="sheetColor" width="400">
+                  <span v-text="symbol" class="ml-2" />
+                  <v-sparkline :value="sparks[symbol]" line-width="1" color="white" />
+                </v-sheet>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+const COLOR = 'cyan darken-4'
+
 export default {
 
   /*
@@ -30,9 +48,11 @@ export default {
   ** DATA
   */
   data: () => ({
-    interval: '240',
-    values: {}, // { ADA: [200, 675, 410, 390, 310, 460, 250, 240] }
+    interval: '60',
+    disableds: {},
+    sparks: {}, // { ADA: [200, 675, 410, 390, 310, 460, 250, 240] }
     colors: {},
+    sheetColor: COLOR,
   }),
 
   /*
@@ -55,24 +75,32 @@ export default {
       const result = await series(symbol, this.interval)
       if (typeof result === 'string') { // error
         this.$store.commit('snackMessage', result)
-        const color = {}
-        color[symbol] = 'accent'
-        this.colors = Object.assign({}, this.colors, color)
+        const disabled = {}
+        disabled[symbol] = true
+        this.disableds = Object.assign({}, this.disableds, disabled)
         return []
       }
       return result
     },
     async spark (symbol) {
+      this.$store.commit('setFlyoutCoin', { symbol })
+      this.$store.commit('openCoinFlyout')
       const data = await this.series(symbol)
       console.log('spark', symbol, data)
       if (data.length) {
         const value = {}
         value[symbol] = data
-        this.values = Object.assign({}, this.values, value)
+        this.sparks = Object.assign({}, this.sparks, value)
+        const color = {}
+        color[symbol] = COLOR
+        this.colors = Object.assign({}, this.colors, color)
       }
     },
     color (symbol) {
-      return this.colors[symbol] || 'cyan darken-4'
+      return this.colors[symbol] || 'accent'
+    },
+    disabled (symbol) {
+      return this.disableds[symbol]
     },
   },
 
